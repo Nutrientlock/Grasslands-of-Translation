b2695f94f6581e967ef764b9c5bbb4fe9ad31897
Mio <kari.bellaros@gmail.com>
2025-12-22 14:54:02 -0800

    1st

---
+++ multi calculation.py
@@ -0,0 +1,12 @@
+def multi_calculation():
+    while True:
+        print('===Multi Calculation Calculator===')
+        print('Choose an option')
+        print('1. Add')
+        print('2. Subtract')
+        print('3. Multiply')
+        print('4. Divide')
+        print("5. Power")
+        pri
+
+

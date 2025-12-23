5dfebacfa8c9bc6b78373c9e4704188b6b520cdd
Mio <kari.bellaros@gmail.com>
2025-12-23 10:57:25 -0800

    12th

---
+++ to_do_list.py
@@ -0,0 +1,17 @@
+while True:
+    print('\n===Welcome===')
+    print('Chosse an Option\n')
+    print('1. Add a Task')
+    print('2. Veiw all Task')
+    print('3. Remove a Task')
+    print('q. Exit\n')
+    choice = input('Enter your choice: ').strip().lower()   
+    if choice == 'q':
+        print('Exiting To Do List. Goodbye!\n')
+        break
+    elif choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass 
